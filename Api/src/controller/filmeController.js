3ba1693b38e8b1059935d2334bfaa58d2e2e444f
milena <milena.custodio.senna@gmail.com>
2022-06-01 22:54:22 -0300

    api parte final

--- Api/src/controller/filmeController.js
+++ Api/src/controller/filmeController.js
@@ -1,8 +1,10 @@
-import { alterarImagem, inserirFilme } from '../repository/filmeRepository.js';
+import { alterarFilme, alterarImagem, buscarPorId, buscarPorNome, inserirFilme, listarTodosFilmes, removerFilme } from '../repository/filmeRepository.js';
 
 import multer from 'multer'
 import { Router } from 'express'
+
 const server = Router();
+const upload = multer({ dest: 'storage/capasFilmes'})
 
 server.post('/filme', async (req, resp) =>{
     try{
@@ -20,10 +22,9 @@ server.post('/filme', async (req, resp) =>{
             if(!novoFilme.lancamento)
             throw new Error('Lancamento do filme é obrigatório!');
 
-            if(!novoFilme.disponivel)
+            if(!novoFilme.disponivel == undefined)
             throw new Error('Campo Disponivel do filme é obrigatória!');
-
-
+            
             if(!novoFilme.usuario)
             throw new Error('usuario nao logado!');
 
@@ -40,11 +41,15 @@ server.post('/filme', async (req, resp) =>{
 })
 
 
-server.put ('/filme/:id/imagem', async (req, resp) =>{
+server.put ('/filme/:id/capa', upload.single('capa'), async (req, resp) =>{
     try {
         const {id} = req.params;
-
-        const resp = await alterarImagem()
+        const imagem = req.file.path;
+        
+        const resposta = await alterarImagem(imagem,id);
+        if (resposta != 1)
+            throw new Error('A imagem não pode ser salva')
+        resp.status(204).send();
 
     }catch(err) {
         resp.status(400).send({
@@ -52,6 +57,111 @@ server.put ('/filme/:id/imagem', async (req, resp) =>{
         })
     }
 
-    })
+})
+
+
+server.get('/filme' , async (req,resp)=> {
+    try{
+        const resposta = await listarTodosFilmes();
+        resp.send(resposta);
+    } catch (err) {
+        resp.status(400).send ({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/busca' , async (req,resp)=> {
+    try{
+        const {nome} = req.query;
+
+        const resposta = await buscarPorNome(nome);
+
+        if(resposta.length==0)
+            resp.status(404).send([])
+        else
+            resp.send(resposta);
+    } catch (err) {
+        resp.status(400).send ({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/:id' , async (req,resp)=> {
+    try{
+        const id = Number(req.params.id);
+        const resposta = await buscarPorId(id);
+
+        if(!resposta)
+            resp.status(404).send([])
+        else
+            resp.send(resposta);
+    } catch (err) {
+        resp.status(400).send ({
+            erro: err.message
+        })
+    }
+})
+
+server.delete('/filme/:id' , async (req,resp)=> {
+    try{
+        const {id} = req.params;
+        
+        const resposta = await removerFilme(id);
+        if(resposta != 1)
+            throw new Error('Filme não pode ser removido.');
+        
+        resp.status(204).send();
+    } catch (err) {
+        resp.status(400).send ({
+            erro: err.message
+        })
+    }
+})
+
+
+server.put('/filme/:id' , async (req,resp)=> {
+    try{
+        const {id} = req.params;
+        const filme =req.body;
+
+        if(!filme.nome)
+            throw new Error('Nome do filme é obrigatório!');
+
+        if(!filme.sinopse)
+            throw new Error('Sinopse do filme é obrigatório!');
+        
+        if(filme.avaliaçao == undefined || filme.avaliaçao<0)
+            throw new Error('Avaliaçao do filme é obrigatória!');
+
+        if(!filme.lancamento)
+            throw new Error('Lancamento do filme é obrigatório!');
+
+        if(!filme.disponivel  == undefined)
+            throw new Error('Campo Disponivel do filme é obrigatória!');
+
+
+        if(!filme.usuario)
+            throw new Error('usuario nao logado!');
+        
+        const resposta = await alterarFilme(id,filme);
+        if(resposta != 1)
+            throw new Error('Filme não pode ser alterado.');
+        else
+            resp.status(204).send();
+
+    } catch (err) {
+        resp.status(400).send ({
+            erro: err.message
+        })
+    }
+})
+
+
+
+
+
+
 
 export default server;
--- Api/src/repository/usuarioRepository.js
+++ Api/src/repository/usuarioRepository.js
@@ -6,7 +6,7 @@ export async function login (email, senha) {
      const comando = 
         `select id_usuario 		id,
                 nm_usuario		nome,
-                ds_email	    email
+                ds_email	          email
            from tb_usuario
            where ds_email 		= ?
            and ds_senha			= ?`
